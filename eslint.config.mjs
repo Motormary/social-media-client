96b77ab4dc01440518a2a4f0fce14576fdc93b19
Mathias <mat.moen@gmail.com>
2024-10-14 13:49:16 +0200

    update: eslint config for jest

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,12 +1,32 @@
 import globals from "globals"
 import pluginJs from "@eslint/js"
+import jest from "eslint-plugin-jest"
 
 export default [
   {
     languageOptions: {
-      globals: { ...globals.browser, ...globals.node },
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
     },
-    ignores: [".github/*", ".husky/*", ".node_modules/*", ".assets/*"],
+  },
+  {
+    files: ["**/*.test.js"],
+    ...jest.configs["flat/recommended"],
+    rules: {
+      ...jest.configs["flat/recommended"].rules,
+      "jest/prefer-expect-assertions": "off",
+    },
+  },
+  {
+    ignores: [
+      ".github/*",
+      ".husky/*",
+      ".node_modules/*",
+      ".assets/*",
+
+    ],
   },
   pluginJs.configs.recommended,
 ]
